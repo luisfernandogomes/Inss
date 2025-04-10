e33716a159d806c7a51d8d8acaa70b2043b7de8a
lf556 <lf5568487@gmail.com>
2025-04-09 21:35:42 -0300

    perdi tudo nessa budega

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def main(page: Page):
     page.navigation_bar = ft.NavigationBar(
         destinations=[
             ft.NavigationBarDestination(icon=ft.Icons.EXPLORE, label="Regras"),
-            ft.NavigationBarDestination(icon=ft.Icons.COMMUTE, label="Simulação"),
+            ft.NavigationBarDestination(icon=ft.Icons.REPORT, label="Simulação"),
             ft.NavigationBarDestination(
-                icon=ft.Icons.BOOKMARK_BORDER,
+                icon=ft.Icons.ACCOUNT_BOX,
                 selected_icon=ft.Icons.BOOKMARK,
                 label="Explore",
             ),
@@ -34,14 +34,14 @@ def main(page: Page):
                 ft.NavigationBarDestination(icon=ft.Icons.REPORT, label="Regras"),
                 ft.NavigationBarDestination(
                     icon=ft.Icons.ACCOUNT_BOX,
-                    selected_icon=ft.Icons.BOOKMARK,
+                    selected_icon=ft.Icons.ACCOUNT_BOX,
                     label="Simulação",
 
                 ),
             ],
             on_change=lambda e: page.go(["/", "/regras", "/simulacao"][e.control.selected_index])
         ), content=ft.Container(),
-        height=500, expand=True,)
+        height=500, expand=True, )
 
     def voltar(e):
         page.views.pop()
@@ -50,22 +50,83 @@ def main(page: Page):
 
     logo = ft.Image(src="senai.png")
 
-    txt_inicio = ft.TextField(value="Bem vindo ao simulador de aposentadoria: INSS",max_lines=3
-                                 ,color=Colors.WHITE,text_size=16,border_radius=40,border_color="cyan")
-
+    txt_inicio = ft.TextField(value="Bem vindo ao simulador de aposentadoria: INSS", max_lines=3
+                              , color=Colors.WHITE, text_size=16, border_radius=40, border_color="cyan")
 
     txt_inicio2 = ft.Text(value="Neste simulador é possivel consultar de acordo com as legalidades do INSS,"
-                                " quando será possivel se aposentar",text_align=CENTER,color=Colors.WHITE, size=14)
-
+                                " quando será possivel se aposentar", text_align=CENTER, color=Colors.WHITE, size=14)
 
     txt_inicio3 = ft.Text(value="Ao navegar a página de regras será possível visualizar quais são os criterios,"
-                                " nos quais permitem que você se aposente",text_align=CENTER,color=Colors.WHITE, size=14)
+                                " nos quais permitem que você se aposente", text_align=CENTER, color=Colors.WHITE,
+                          size=14)
 
     txt_inicio4 = ft.Text(value="Após visualizar quais são os criterios,"
                                 " prossiga para a pagina de simulação, aonde você deverá informar as informações"
-                                " necessaria",text_align=CENTER,color=Colors.WHITE, size=14)
+                                " necessaria", text_align=CENTER, color=Colors.WHITE, size=14)
 
 
+    idade = ft.TextField(label='informe sua quantos anos vc tem',
+                         border_color=Colors.CYAN,
+                         border_width=2,
+                         border_radius=10,
+                         focused_border_color=Colors.RED
+                         )
+    genero = ft.RadioGroup(content=ft.Row([
+        ft.Radio(value='homem', label="Homem"),
+        ft.Radio(value='mulher', label="Mulher"),
+    ]))
+
+    tempo_contribuicao = ft.TextField(label='Tempo de contribuição', border_color=Colors.CYAN, border_width=2,
+                                      border_radius=10, focused_border_color=Colors.RED, )
+
+    media_salarial = ft.TextField(label='Média salarial', border_color=Colors.CYAN, border_width=2,
+                                  border_radius=10, focused_border_color=Colors.RED, )
+
+    opcao = ft.RadioGroup(content=ft.Row([
+        ft.Radio(value='tempo', label="Tempo de contribuição"),
+        ft.Radio(value='idade', label="Idade")
+    ]))
+
+    txt_resultado = ft.Text('a')
+    def simular_aposentadoria(e):
+        if opcao.value == "tempo":
+            if genero.value == "homem":
+                try:
+                    idade_int = int(idade.value)
+                except ValueError:
+                    txt_resultado.value = 'insira um valor numerico'
+                    page.go("/resultado")
+                    return
+                try:
+                    tempo_contri_int = int(tempo_contribuicao.value)
+                except ValueError:
+                    txt_resultado.value = 'insira um valor numerico'
+                    page.go("/resultado")
+                    return
+                try:
+                    media_salarial_int = float(media_salarial.value)
+                except ValueError:
+                    txt_resultado.value = 'insira um valor numerico'
+                    page.go("/resultado")
+                    return
+                if media_salarial_int < 1100:
+                    txt_resultado.value = 'Média salarial inserida não é valida'
+                    page.go("/resultado")
+                if tempo_contri_int < 35:
+
+                    txt_resultado.value = ('aida não é possivel se aposentar seguindo as diretrizes de tempo de'
+                                           ' contribuição')
+
+            elif genero.value == "mulher":
+
+        elif opcao.value == "idade":
+            if genero.value == "homem":
+
+            elif genero.value == "mulher":
+
+
+
+        # page.go("/resultado")
 
     def gerenciar_rotas(e):
         page.views.clear()
@@ -73,63 +134,27 @@ def main(page: Page):
             View(
                 "/",
                 [
-                    AppBar(leading=logo, leading_width=100, center_title=True, title=Text("home"), bgcolor=Colors.CYAN, color=Colors.BLACK,
+                    AppBar(leading=logo, leading_width=100, center_title=True, title=Text("home"), bgcolor=Colors.CYAN,
+                           color=Colors.BLACK,
                            actions=[
                                ft.PopupMenuButton(items=
                                [
                                    ft.PopupMenuItem(text="Regras", on_click=lambda _: page.go("/regras")),
-                                   ft.PopupMenuItem(text="Simulação",on_click=lambda _: page.go("/simulacao")),
+                                   ft.PopupMenuItem(text="Simulação", on_click=lambda _: page.go("/simulacao")),
 
                                ])
 
-                               #    ElevatedButton(text="Regras", on_click=lambda _: page.go("/regras")),
-                               # ElevatedButton(text="Simulação", on_click=lambda _: page.go("/simulacao")),
+
                            ]),
-                    # ft.VerticalDivider(width=9, thickness=3),
 
-                    # ft.ResponsiveRow(
-                    #     [
-                    #
-                    #         ElevatedButton(text="Regras", on_click=lambda _: page.go("/regras")),
-                    #         ElevatedButton(text="Simulação", on_click=lambda _: page.go("/simulacao")),
-                    #         ft.ProgressBar(width=400, color="WHITE", bgcolor="PINK"),
-                    #         txt_resultado,
-                    #
-                    #     ],
-                    #     run_spacing={"xs": 10},
-                    #
-                    # ),
-                    ft.Card(
-                        content=ft.Container(
-                            content=ft.Column(
-                                [
-                                    ft.ListTile(
-                                        leading=logo,
-                                        title=ft.Text('Bem vindo')
 
-                                    ),
-                                    ft.Column(
-                                        [
-
-                                            txt_inicio,
-                                            txt_inicio2,
-                                            txt_inicio3,
-                                            txt_inicio4,
-                                        ]
-                                    )
-                                ]
-                            ),
-                            height=500,
-                        )
-
-                    ),
+                    ft.ResponsiveRow([
+                        txt_inicio, txt_inicio2, txt_inicio3, txt_inicio4,
+                    ]
+                        , vertical_alignment=CENTER, ),
 
                     pagelet
 
-                    # ft.Icon(name=ft.Icons.FAVORITE, color=ft.Colors.TERTIARY_CONTAINER),
-                    # ft.Text("Linear progress indicator", style="headlineSmall"),
-                    # ft.Column([ft.Text("Doing something..."), pb]),
-                    # ft.Text("Indeterminate progress bar", style="headlineSmall"),
 
                 ]
             )
@@ -140,16 +165,46 @@ def main(page: Page):
             page.views.append(
                 View(
                     "/regras", [
-                        AppBar(title=Text("regras"),leading=logo, bgcolor=Colors.CYAN, color=Colors.BLACK, center_title=True),
-                        ft.ResponsiveRow(
-                            [
-                                ElevatedButton(text="Inicio", on_click=lambda _: page.go("/"), ),
-                                ElevatedButton(text="Simulação", on_click=lambda _: page.go("/simulacao")),
 
-                            ]
-                        )
+
+                        AppBar(title=Text("regras"), leading=logo, bgcolor=Colors.CYAN, color=Colors.BLACK,
+                               center_title=True, ),
+
+                        Text(value="Regras Básicas de Aposentadoria:", color=Colors.WHITE,
+                                 size=10, expand=True),
+                        Text(value="Aposentadoria por Idade:",color=Colors.WHITE,size=10,
+                                  expand=True),
+
+                        ft.Row(
+                            [
+                                ft.Text('Homens:', size=10, expand=True),
+                                ft.Text('Homens: 65 anos de idade e pelo menos 15 anos de contribuição.', size=9,
+                                        expand=True),
+                            ],
+                            expand=True
+                        ),
+
+                        ft.Row([Text('Mulheres:', size=10, expand=True),
+                                Text('Mulheres: 62 anos de idade e pelo menos'
+                                     ' 15 anos de contribuição.', size=9, expand=True)], expand=True),
+
+                        Text('Aposentadoria por Tempo de Contribuição:', expand=True),
+
+                        ft.Row([Text('Homens:', expand=True, ),
+                                Text('35 anos de contribuição.', size=9, expand=True), ], expand=True),
+
+                        ft.Row([Text('Mulheres:', expand=True, ),
+                                Text('Mulheres: 30 anos de contribuição.', size=9, expand=True), ], expand=True),
+
+                        Text('Valor Estimado do Benefício:',  expand=True, ),
+                        ft.Text('O valor da aposentadoria será uma média'
+                                                             ' de 60% da média salarial informada,'
+                                                             ' acrescido de 2% por ano que exceder'
+                                                             ' o tempo mínimo de contribuição.',expand=True, size=8),
+                        pagelet
 
                     ]
+
                 )
             )
 
@@ -157,10 +212,8 @@ def main(page: Page):
             page.views.append(
                 View(
                     "/resultado", [
-                        AppBar(title=Text("resultado"), bgcolor=Colors.SECONDARY_CONTAINER),
-                        ElevatedButton(text="Inicio", on_click=lambda _: page.go("/")),
-                        ElevatedButton(text="Regras", on_click=lambda _: page.go("/regras")),
-                        ElevatedButton(text="Regras", on_click=lambda _: page.go("/simulacao"))
+                        AppBar(title=Text("resultado"), center_title=True, bgcolor=Colors.CYAN, color=Colors.BLACK,),
+                        txt_resultado,
 
                     ]
                 )
@@ -170,16 +223,30 @@ def main(page: Page):
             page.views.append(
                 View(
                     "/simulacao", [
-                        AppBar(title=Text("simulacao"), bgcolor=Colors.SECONDARY_CONTAINER),
+                        AppBar(title=Text("simulacao"), leading=logo, center_title=True, bgcolor=Colors.CYAN,
+                               color=Colors.BLACK, ),
                         # tenho que mudar esse butão que direciona para resultado para dentro de uma função
-                        ElevatedButton(text="resultado", on_click=lambda _: page.go("/resultado")),
-                        ElevatedButton(text="Inicio", on_click=lambda _: page.go("/inicio"))
+                        idade,
+
+                        genero,
+
+                        tempo_contribuicao,
+                        media_salarial,
+
+                        opcao,
+
+                        ft.FilledButton(text='Simular',icon=ft.Icons.ACCOUNT_CIRCLE, on_click=simular_aposentadoria),
+
+
+
+                        pagelet,
 
                     ]
                 )
             )
 
         page.update()
+
 
     page.on_route_change = gerenciar_rotas
     page.on_view_pop = voltar
